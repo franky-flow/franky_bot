f779ffd6432a9d32de8fdd78efa3fb336d2116b1
Francisco José González <fjgonzalez@telecoming.com>
2025-12-30 15:12:31 +0100

    adaptaciones para robot_real con arduino

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -7,6 +7,10 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
@@ -42,26 +46,6 @@ def generate_launch_description():
         description='World to load'
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
-    )
-
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-                    launch_arguments={'gz_args': ['-r -s -v4 --render-engine ogre ', world], 'on_exit_shutdown': 'true'}.items()
-                 #   launch_arguments={'gz_args': '-r -s -v 4 --render-engine ogre ' + world}.items()
-    )
-
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                    '-name', 'franky_robot',
-                                   '-z', '0.1'],
-                        output='screen')
 
     # Bridge between Gazebo and ROS2 topics
     # This bridges /cmd_vel from ROS2 to Gazebo and /odom from Gazebo to ROS2
@@ -79,6 +63,20 @@ def generate_launch_description():
         output='screen'
     )
 
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+
+    # incluimos delay para que al arrancar pueda funcionar el robot_state_publisher y cargar el robot en gazebo
+    delayed_controller_manager = TimerAction(period=3.0,actions=[controller_manager])
+
     # Controllers de los componentes hardware
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -86,10 +84,24 @@ def generate_launch_description():
         arguments=["diff_cont"],
     )
 
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
     )
 
     joy_params = os.path.join(get_package_share_directory('franky_bot'),'config','joystick.yaml')
@@ -109,10 +121,9 @@ def generate_launch_description():
     return LaunchDescription([
         world_arg,
         rsp,
-        gazebo,
-        spawn_entity,
         bridge,
-        diff_drive_spawner,
-        joint_broad_spawner,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
         teleop_node
     ])
